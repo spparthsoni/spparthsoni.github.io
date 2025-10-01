4a8472c8ffea820b8d7dce4bc1ee2cea81a8665d
parth <spparthsoni@gmail.com>
2025-10-01 14:29:11 +0530

    Remove Instagram icon from footer social buttons
    - Removed Instagram SVG icon and link
    - Updated GitHub link to actual profile URL
    - Footer now shows only: LinkedIn and GitHub
    - Cleaner, minimal footer design
    - Focus on professional networking platforms

--- assets/js/app.js
+++ assets/js/app.js
@@ -186,18 +186,7 @@ let footer = $(`
 
 
 
-    <a class="social-button instagram" href="" target="_blank" >
-    <!-- svg code for instagram icon -->
-      <svg class="instagram-icon-footer" x="0px" y="0px" viewBox="0 0 202.5 202.5"
-        style="enable-background:new 0 0 202.5 202.5;">
-        <circle id="littleCircle" class="st0" cx="101" cy="101.5" r="18.9" />
-        <circle id="shutter" class="st0" cx="101" cy="101.5" r="8" />
-        <circle id="lens" class="st0" cx="125.5" cy="78.6" r="2.9" />
-        <path id="camera" class="st0" d="M79,60.5h44c10.5,0,19,8.5,19,19v44c0,10.5-8.5,19-19,19H79c-10.5,0-19-8.5-19-19v-44                                                                            C60,69,68.5,60.5,79,60.5z" />
-      </svg>
-    </a>
-
-    <a class="social-button github" href="#" target="_blank">
+    <a class="social-button github" href="https://github.com/spparthsoni" target="_blank">
     <!-- SVG code for Github icon -->
     <svg class="github-icon-footer" width="45px" height="45px" viewBox="0 0 300 300">
         <!-- body -->
